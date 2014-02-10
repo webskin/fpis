1219efed1569aba0f5ea80c1bba147007f17940e
Mickael Gauvin <mickael.gauvin@gmail.com>
2014-02-10 15:05:22 +0100

    Exo1

--- src/main/scala/Exo1Fibo.scala
+++ src/main/scala/Exo1Fibo.scala
@@ -1,16 +1,38 @@
+package fpis
+
 /**
  * Created by Mickaël Gauvin on 2/10/14.
  */
 object Exo1Fibo {
 
+  /*
   def fib(n: Int): Int = {
     def localFib(a: Int, b: Int, remaining: Int): Int = {
       if (remaining == 0) {
-        a
+        b
       } else {
-        localFib(a + b, b, remaining - 1)
+        localFib(b, a + b, remaining - 1)
       }
     }
-    localFib(0, 1, n)
+    if (n <= 0) {
+      0
+    } else if (n == 1) {
+      1
+    } else {
+      localFib(1, 1, n - 2)
+    }
+  }
+  */
+
+  // soluce
+  // 0 and 1 are the first two numbers in the sequence,
+  // so we start the accumulators with those.
+  // At every iteration, we add the two numbers to get the next one.
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def loop(n: Int, prev: Int, cur: Int): Int =
+      if (n == 0) prev
+      else loop(n - 1, cur, prev + cur)
+    loop(n, 0, 1)
   }
 }
--- src/test/scala/Exo1FiboSpec.scala
+++ src/test/scala/Exo1FiboSpec.scala
@@ -1,18 +1,41 @@
-import org.spec2.mutable._
+package fpis
+
+import org.specs2.mutable._
+
+import Exo1Fibo._
 
 /**
  * Created by Mickaël Gauvin on 2/10/14.
  */
 class Exo1FiboSpec extends Specification {
-  "The 'Hello world' string" should {
-    "contain 11 characters" in {
-      "Hello world" must have size(11)
+
+  "fib(0)" should {
+    "be equal to 0" in {
+      fib(0) must be equalTo(0)
     }
-    "start with 'Hello'" in {
-      "Hello world" must startWith("Hello")
+  }
+
+  "fib(1)" should {
+    "be equal to 1" in {
+      fib(1) must be equalTo(1)
     }
-    "end with 'world'" in {
-      "Hello world" must endWith("world")
+  }
+
+  "fib(2)" should {
+    "be equal to 1" in {
+      fib(2) must be equalTo(1)
+    }
+  }
+
+  "fib(3)" should {
+    "be equal to 2" in {
+      fib(3) must be equalTo(2)
+    }
+  }
+
+  "fib(6)" should {
+    "be equal to 8" in {
+      fib(6) must be equalTo(8)
     }
   }
 }
