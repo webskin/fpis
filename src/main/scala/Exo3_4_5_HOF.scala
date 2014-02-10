ba39284c40068158be3e1a7a5c92a9eac2da6969
Mickael Gauvin <mickael.gauvin@gmail.com>
2014-02-10 17:51:18 +0100

    Exo 3 4 5

--- src/main/scala/Exo3_4_5_HOF.scala
+++ src/main/scala/Exo3_4_5_HOF.scala
@@ -10,6 +10,13 @@ object Exo3_4_5_HOF {
 
   def uncurry[A,B,C](f: A => B => C): (A, B) => C = (a, b) => f(a)(b)
 
+  /**
+   * This is such a common thing to want to do that Scala's standard library provides
+   * compose as a method on Function1 (the interface for functions that take one
+   * argument). To compose two functions f and g , you simply say f compose g 9 .
+   * It also provides an andThen method. f andThen g is the same as g
+   * compose f
+   */
   def compose[A,B,C](f: B => C, g: A => B): A => C = a => f(g(a))
 
 }
