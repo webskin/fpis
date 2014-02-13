e3461bab2744918706fba525e848765069f2a14a
Mickael Gauvin <mickael.gauvin@gmail.com>
2014-02-13 10:05:03 +0100

    state

--- src/main/scala/Stream.scala
+++ src/main/scala/Stream.scala
@@ -147,17 +147,6 @@ object Stream {
   // could also of course be implemented as constant(1)
   val onesViaUnfold = unfold(1)(_ => Some((1,1)))
 
-  /*
-  s.startsWith(s2) when corresponding elements of s and s2 are all equal, until the point that s2 is exhausted. If s is exhausted first, or we find an element that doesn't match, we terminate early. Using non-strictness, we can compose these three separate logical steps - the zipping, the termination when the second stream is exhausted, and the termination if a nonmatching element is found or the first stream is exhausted.
-  */
-  def startsWith[A](s1: Stream[A], s: Stream[A]): Boolean =
-    s1.zipAll(s).takeWhile(!_._2.isEmpty) forAll {
-      case (Some(h),Some(h2)) if h == h2 => true
-      case _ => false
-    }
-
-  def hasSubsequence[A](s1: Stream[A], s2: Stream[A]): Boolean =
-    s1.tails exists (startsWith(_,s2))
 }
 
 // In this alternate version of `Stream`, the abstract method is `foldRight`
