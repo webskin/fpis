4920f8a8a5df8ed67fe09deceba798708cc83603
Mickael Gauvin <mickael.gauvin@gmail.com>
2014-02-17 14:35:56 +0100

    state

--- src/main/scala/State.scala
+++ src/main/scala/State.scala
@@ -28,33 +28,139 @@ object RNG {
       (f(a), rng2)
     }
 
-  def positiveInt(rng: RNG): (Int, RNG) = ???
+  // We need to be quite careful not to skew the generator.
+  // Since `Int.Minvalue` is 1 smaller than `-(Int.MaxValue)`,
+  // it suffices to increment the negative numbers by 1 and make them positive.
+  // This maps Int.MinValue to Int.MaxValue and -1 to 0.
+  def positiveInt(rng: RNG): (Int, RNG) = {
+    val (i, r) = rng.nextInt
+    (if (i < 0) -(i + 1) else i, r)
+  }
 
-  def double(rng: RNG): (Double, RNG) = ???
+  // We generate a positive integer and divide it by one higher than the
+  // maximum. This is just one possible solution.
+  def double(rng: RNG): (Double, RNG) = {
+    val (i, r) = positiveInt(rng)
+    (i / (Int.MaxValue.toDouble + 1), r)
+  }
 
-  def intDouble(rng: RNG): ((Int,Double), RNG) = ???
+  def boolean(rng: RNG): (Boolean, RNG) =
+    rng.nextInt match { case (i,rng2) => (i%2==0,rng2) }
 
-  def doubleInt(rng: RNG): ((Double,Int), RNG) = ???
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i, r1) = rng.nextInt
+    val (d, r2) = double(r1)
+    ((i, d), r2)
+  }
 
-  def double3(rng: RNG): ((Double,Double,Double), RNG) = ???
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val ((i, d), r) = intDouble(rng)
+    ((d, i), r)
+  }
 
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1, r1) = double(rng)
+    val (d2, r2) = double(r1)
+    val (d3, r3) = double(r2)
+    ((d1, d2, d3), r3)
+  }
 
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+  // There is something terribly repetitive about passing the RNG along
+  // every time. What could we do to eliminate some of this duplication
+  // of effort?
 
-  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
+  // A simple recursive solution
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) =
+    if (count == 0)
+      (List(), rng)
+    else {
+      val (x, r1)  = rng.nextInt
+      val (xs, r2) = ints(count - 1)(r1)
+      (x :: xs, r2)
+    }
 
-  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
+  // A tail-recursive solution
+  def ints2(count: Int)(rng: RNG): (List[Int], RNG) = {
+    def go(count: Int, r: RNG, xs: List[Int]): (List[Int], RNG) =
+      if (count == 0)
+        (List(), r)
+      else {
+        val (x, r2) = r.nextInt
+        go(count - 1, r2, x :: xs)
+      }
+    go(count, rng, List())
+  }
+
+
+  val _double: Rand[Double] = map(positiveInt)(i => i / (Int.MaxValue.toDouble + 1))
+
+  // This implementation of map2 passes the initial RNG to the first argument
+  // and the resulting RNG to the second argument. It's not necessarily wrong
+  // to do this the other way around, since the results are random anyway.
+  // We could even pass the initial RNG to both `f` and `g`, but that might
+  // have unexpected results. E.g. if both arguments are `RNG.int` then we would
+  // always get two of the same `Int` in the result. When implementing functions
+  // like this, it's important to consider how we would test them for
+  // correctness.
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    rng => {
+      val (a, r1) = ra(rng)
+      val (b, r2) = rb(r1)
+      (f(a, b), r2)
+    }
+
+  def both[A,B](ra: Rand[A], rb: Rand[B]): Rand[(A,B)] =
+    map2(ra, rb)((_, _))
+
+  val randIntDouble: Rand[(Int, Double)] =
+    both(int, double)
+
+  val randDoubleInt: Rand[(Double, Int)] =
+    both(double, int)
+
+  // In `sequence`, the base case of the fold is a `unit` action that returns
+  // the empty list. At each step in the fold, we accumulate in `acc`
+  // and `f` is the current element in the list.
+  // `map2(f, acc)(_ :: _)` results in a value of type `Rand[List[A]]`
+  // We map over that to prepend (cons) the element onto the accumulated list.
+  //
+  // We are using `foldRight`. If we used `foldLeft` then the values in the
+  // resulting list would appear in reverse order. It would be arguably better
+  // to use `foldLeft` followed by `reverse`. What do you think?
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+    fs.foldRight(unit(List[A]()))((f, acc) => map2(f, acc)(_ :: _))
+
+  // It's interesting that we never actually need to talk about the `RNG` value
+  // in `sequence`. This is a strong hint that we could make this function
+  // polymorphic in that type.
+
+  def _ints(count: Int): Rand[List[Int]] =
+    sequence(List.fill(count)(int))
+
+  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
+    rng => {
+      val (a, r1) = f(rng)
+      g(a)(r1) // We pass the new state along
+    }
+
+  def positiveLessThan(n: Int): Rand[Int] = {
+    flatMap(positiveInt) { i =>
+      val mod = i % n
+      if (i + (n-1) - mod > 0) unit(mod) else positiveLessThan(n)
+    }
+  }
+
+  def _map[A,B](s: Rand[A])(f: A => B): Rand[B] =
+    flatMap(s)(a => unit(f(a)))
+
+  def _map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    flatMap(ra)(a => map(rb)(b => f(a, b)))
 }
 
-case class State[S,+A](run: S => (A, S)) {
-  def map[B](f: A => B): State[S, B] =
-    sys.error("todo")
-  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
-    sys.error("todo")
-  def flatMap[B](f: A => State[S, B]): State[S, B] =
-    sys.error("todo")
-}
+import State._
+
+
+
 
 sealed trait Input
 case object Coin extends Input
@@ -62,7 +168,117 @@ case object Turn extends Input
 
 case class Machine(locked: Boolean, candies: Int, coins: Int)
 
+/*
+<adelbertc> webskin - the _ <- is certainly confusing, but recall that state has two params, State[S, A], where S is the
+state, and A is value computed from the state. S is "fixed" in the flatMap/map, the "A" is what varies. that is, map has
+ type State[S, A] => (A => B) => State[S, B]. If you say, do a "put" on a state, you get State[S, Unit], because there
+  is no meaningful value to return, so often you see _ <- s.put or something. THe _ is "ignoring" the
+ unit part of the state, but under teh covers the plumbing is being done to propagate the new state to the "S" part
+
+ Exemple d'utilisation :
+  Candy.simulateMachine(List(Coin, Turn))
+  res5.run(Machine(false, 10, 10))
+  ((Int, Int), fpinscala.state.Machine) = ((10,9),Machine(true,9,10))
+
+ */
+object Candy {
+  
+
+  def inputReaction(i: Input): State[Machine, Unit] = {
+    modify((s: Machine) => (i, s) match {
+      case (_, Machine(_, 0, _)) => s // no candies => state does not change
+      case (Coin, Machine(false, _, _)) => s // the machine has already a coin => state does not change
+      case (Turn, Machine(true, _, _)) => s // the machine is locked  => state does not change
+      case (Coin, Machine(true, candy, coin)) =>
+        Machine(false, candy, coin + 1)
+      case (Turn, Machine(false, candy, coin)) =>
+        Machine(true, candy - 1, coin)
+    })
+  }
+  
+  def inputReactions(inputs: List[Input]): List[State[Machine, Unit]] = inputs map inputReaction
+
+  def simulateMachine2(inputs: List[Input]): State[Machine, (Int, Int)] = for {
+    _ <- sequence(inputReactions(inputs)) // Unit is extracted
+    s <- get
+  } yield (s.coins, s.candies)
+
+  def simulateMachine3(inputs: List[Input]): State[Machine, (Int, Int)] =
+    sequence(inputReactions(inputs)).flatMap(_ => get.map(m => (m.coins, m.candies)))
+
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = for {
+    _ <- sequence(inputs.map(i => modify((s: Machine) => (i, s) match {
+      case (_, Machine(_, 0, _)) => s
+      case (Coin, Machine(false, _, _)) => s
+      case (Turn, Machine(true, _, _)) => s
+      case (Coin, Machine(true, candy, coin)) =>
+        Machine(false, candy, coin + 1)
+      case (Turn, Machine(false, candy, coin)) =>
+        Machine(true, candy - 1, coin)
+    })))
+    s <- get
+  } yield {
+    print(s.candies + " " + s.coins )
+    (s.coins, s.candies)
+  }
+
+}
+
+/*case class State[S,+A](run: S => (A, S)) {
+
+
+  def map[B](f: A => B): State[S, B] =
+    State(s => (f(a), s))
+  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+    sys.error("todo")
+  def flatMap[B](f: A => State[S, B]): State[S, B] = State(s => {
+    val (a, s1) = run(s)
+    f(a)
+  })
+}*/
+case class State[S, +A](run: S => (A, S)) {
+  def map[B](f: A => B): State[S, B] =
+    flatMap(a => unit(f(a)))
+  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+    flatMap(a => sb.map(b => f(a, b)))
+  def map2_[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+    for {
+      a <- this
+      b <- sb
+    } yield f(a, b)
+  def flatMap[B](f: A => State[S, B]): State[S, B] = State(s => {
+    val (a, s1) = run(s)
+    f(a).run(s1)
+  })
+}
+
 object State {
   type Rand[A] = State[RNG, A]
-  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
+
+  def unit[S, A](a: A): State[S, A] = State(s => (a, s))
+
+  def modify[S](f: S => S): State[S, Unit] = for {
+    s <- get // Gets the current state and assigns it to `s`.
+    _ <- set(f(s)) // Sets the new state to `f` applied to `s`.
+  } yield ()
+
+  def modifify2[S](f: S => S): State[S, Unit] = get.flatMap(s => set(f(s)).map(ignoredVar => ()))
+  def modifify3[S](f: S => S): State[S, Unit] = get.flatMap(s => set(f(s)).map(_ => ()))
+
+  def get[S]: State[S, S] = State(s => (s, s))
+
+  def set[S](s: S): State[S, Unit] = State(_ => ((), s))
+
+  // This implementation uses a loop internally and is the same recursion
+  // pattern as a left fold. It is quite common with left folds to build
+  // up a list in reverse order, then reverse it at the end.
+  // (We could also use a collection.mutable.ListBuffer internally.)
+  def sequence[S, A](sas: List[State[S, A]]): State[S, List[A]] = {
+    def go(s: S, actions: List[State[S,A]], acc: List[A]): (List[A],S) =
+      actions match {
+        case Nil => (acc.reverse,s)
+        case h :: t => h.run(s) match { case (a,s2) => go(s2, t, a :: acc) }
+      }
+    State((s: S) => go(s,sas,List()))
+  }
 }
